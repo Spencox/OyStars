f8a6b2ead3f5c9e2e7c760ef3234e0d7ec6fd227
Angie <angelam202@hotmail.com>
2023-10-30 18:23:16 -0500

    commit b4 mergin fea/main-page-html into dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const searchButton = document.getElementById("search-oysters")
-searchButton.addEventListener("click", funtion() {
-    window.location.href = "search-page.html"
+const searchButton = document.getElementById("search-oysters");
+searchButton.addEventListener("click", function() {
+    window.location.href = "search-page.html";
 });
