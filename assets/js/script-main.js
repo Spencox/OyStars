c76d63740220b0a15d595d416b1d4bf48e1e8d80
Angie <angelam202@hotmail.com>
2023-11-04 14:43:07 -0500

    organize files, create js and css files for main and search page each

---
+++ assets/js/script-main.js
@@ -0,0 +1,6 @@
+// main page search button
+const searchButton = document.querySelector(".search-oysters")
+searchButton.addEventListener("click", function() {
+    document.location.href = "search-page.html"
+});
+
--- assets/js/script-search.js
+++ assets/js/script-search.js
@@ -1,10 +1,3 @@
-
-// main page search button
-const searchButton = document.querySelector(".search-oysters")
-searchButton.addEventListener("click", function() {
-    window.location.href = "search-page.html"
-});
-
 const sampleResults = {
   "businesses": [
     {
